de603e10ce9519bcd8db3b756d1142c2b19cfb43
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2024-12-31 12:03:04 +0530

    Create Selection-sort.java

---
+++ lab-0/Sorting with time/Selection-sort.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class Ssort{
+	public static void main(String[] args){	
+    long start = System.nanoTime();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("enter no of elements:");
+		int n = sc.nextInt();
+		
+		int[] a = new int[n];
+		System.out.print("enter elements:");
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		ssort(a);
+		System.out.println("sorted array is :");
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]+"");
+    
+	  long end = System.nanoTime();
+		double time = (end - start) / 1000000.0;
+		System.out.println("\nTime taken: " + time + " milliseconds");
+  }
+	
+static void ssort(int[] a){
+
+	int i,j,temp,minpos; 
+	int n=a.length;
+	for(i=0; i<=(n-2); i++) 
+
+	{ 
+
+		minpos = i; 
+
+		for(j=i+1; j<=(n-1); j++) 
+
+		{ 
+
+		    if(a[j]<a[minpos])
+		    {
+		     minpos=j; 
+		    } 
+
+		} 
+
+		temp = a[i]; 
+
+		a[i] = a[minpos]; 
+
+		a[minpos] = temp; 
+
+	 } 
+
+	}
+} 
